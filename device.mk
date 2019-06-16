de05b267844eba9f563f0d623c7b05c200cf798b
Nick-Infinity <nikhilg.me@gmail.com>
2019-06-16 17:26:21 +0000

    Bump health to 2.0

--- device.mk
+++ device.mk
@@ -74,6 +74,10 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0_system
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-service
 	
 # IMS
 PRODUCT_PACKAGES += \
